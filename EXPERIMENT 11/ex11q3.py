d60c12c071460e5829cc9ef60b8dd535298bcbbb
prabhu-0001 <24cseaiml019.prabhupritampradhan@giet.edu>
2026-03-23 08:47:15 +0530

    DeletE

--- EXPERIMENT 11/ex11q3.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-arr=np.array([1,2,3,4])
-print(f"Max : {arr.max()}\nMin :{arr.min()}\nSum :{arr.sum()}\nproduct : {arr.prod()}\nBroadcasted result:{arr+5}")
--- EXPERIMENT 11/ex11q6.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-data={'Name':['Alice',"Bob",'Charlie'],
-      'Age':[25,30,35],
-      'City':['New york','San fransisco','Los angles']}
-df=pd.DataFrame(data);
-print("Data Frame : \n",df)
-print("Age column : ",df['Age'])
-print("Row at index 1 : ",df.iloc[1])
