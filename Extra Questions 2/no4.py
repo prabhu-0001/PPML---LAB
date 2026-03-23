12dd61304bafe6f7759c370337085a907c6c4aa1
prabhu-0001 <24cseaiml019.prabhupritampradhan@giet.edu>
2026-03-23 08:44:47 +0530

    Delete

--- Extra Questions 2/no4.py
+++
@@ -1,7 +0,0 @@
-def reverse(s):
-    if len(s) == 0:
-        return s
-    return reverse(s[1:])+s[0]
-
-s = input("Enter a string :- ")
-print("Its reverse is ",reverse(s))
--- Extra Questions 2/no7.py
+++
@@ -1,8 +0,0 @@
-def palindrome_check(s):
-    if len(s) <= 1:
-        return True
-    if s[0] != s[-1]:
-        return False
-    return palindrome_check(s[1:-1])
-
-print(palindrome_check("pop"))  
